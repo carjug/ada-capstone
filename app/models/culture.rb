57694b171b50f342bb1d6d4e4bfa7e2a3a13a0b0
Carly Jugler <carjug@gmail.com>
2015-10-18 15:05:14 -0700

    Adds comments to culture model

--- app/models/culture.rb
+++ app/models/culture.rb
@@ -4,9 +4,9 @@ class Culture < ActiveRecord::Base
 
   # Scopes
 
-  scope :top_culture_by_place, -> (place) {
-    joins(:users).where('place.ratings.user_id')
-  }
+  # scope :top_culture_by_place, -> (place) {
+  #   joins(:users).where('place.ratings.user_id')
+  # }
 
   # def top_culture_by_place(place)
   #   self.users
