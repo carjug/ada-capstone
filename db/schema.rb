0f135db2043e790be49417313e2b6fd40f7f0d24
Carly Jugler <carjug@gmail.com>
2015-10-28 14:28:59 -0700

    schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027194659) do
+ActiveRecord::Schema.define(version: 20151028204427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,24 +22,24 @@ ActiveRecord::Schema.define(version: 20151027194659) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories_places", id: false, force: :cascade do |t|
-    t.integer "place_id",    null: false
-    t.integer "category_id", null: false
+  create_table "categories_places", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "place_id"
   end
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
+    t.string   "lat_long"
     t.integer  "prov_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "lat_long"
   end
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
+    t.string   "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "code"
   end
 
   create_table "place_types", force: :cascade do |t|
@@ -50,32 +50,32 @@ ActiveRecord::Schema.define(version: 20151027194659) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
+    t.string   "image"
     t.integer  "city_id"
+    t.integer  "place_type_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.integer  "place_type_id"
-    t.string   "image"
   end
 
   create_table "provs", force: :cascade do |t|
     t.string   "name"
+    t.string   "abbreviation"
     t.integer  "country_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "abbreviation"
   end
 
   create_table "questions", force: :cascade do |t|
+    t.string   "question"
     t.string   "option1"
     t.string   "option2"
     t.string   "option3"
     t.string   "option4"
     t.string   "option5"
-    t.integer  "place_type_id"
     t.string   "cat_or_ord"
+    t.integer  "place_type_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "question"
   end
 
   create_table "responses", force: :cascade do |t|
@@ -88,23 +88,23 @@ ActiveRecord::Schema.define(version: 20151027194659) do
   end
 
   create_table "user_recommendations", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "place_id"
-    t.string   "score"
     t.integer  "rank"
+    t.string   "place_name"
+    t.string   "score"
+    t.integer  "user_id"
+    t.integer  "city_id"
+    t.integer  "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "city_id"
-    t.string   "place_name"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "email"
     t.string   "password_digest"
+    t.integer  "city_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "email"
-    t.integer  "city_id"
   end
 
 end
