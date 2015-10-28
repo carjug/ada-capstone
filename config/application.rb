9e919cc042a3d9fa2a43cdc53e956cbb513a0bcc
Carly Jugler <carjug@gmail.com>
2015-10-28 14:46:23 -0700

    Adds assets compiler

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module AdaCapstone
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
-
+    config.assets.compile = true
 
   end
 end
