e0c57198e3c9971c10d951828db7f679ed9f1562
Carly Jugler <carjug@gmail.com>
2015-10-28 18:39:32 -0700

    Adds whenever jobs for production

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,14 +4,31 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 
 # Background offline jobs
 
-set :output, "#{Rails.root}/log/cron.log"
+@environment = Rails.env
 
-every 10.minutes do
-  command "/usr/local/bin/python #{Rails.root}/lib/recommender/py_recommender.py"
+if @environment == "development"
+
+  set :output, "#{Rails.root}/log/cron.log"
+
+  every 10.minutes do
+    command "/usr/local/bin/python #{Rails.root}/lib/recommender/py_recommender.py"
+  end
+
+  every 15.minutes do
+    runner "Place.write_csv"
+    runner "User.create_user_recommendations"
+  end
 end
 
-every 15.minutes do
-  runner "Place.write_csv"
-  runner "User.create_user_recommendations"
-end
+if @environment == "production"
+  set :output, "#{Rails.root}/log/production.log"
 
+  every 5.minutes do
+    command "/usr/local/bin/python #{Rails.root}/lib/recommender/py_recommender.py"
+  end
+
+  every 1.minutes do
+    runner "Place.write_csv"
+    runner "User.create_user_recommendations"
+  end
+end
