62658f4ad27bbece71c468ca44c24a3d5d46fa56
Carly Jugler <carjug@gmail.com>
2015-10-30 12:17:58 -0700

    Adds heroku scheduler rake tasks

--- lib/recommender/py_recommender.py
+++ lib/recommender/py_recommender.py
@@ -8,14 +8,14 @@ from boto.s3.key import Key
 from dotenv import load_dotenv
 
 # this is for local reading
-path = os.path.dirname(os.path.abspath(__file__))
-data = gl.SFrame.read_csv(os.path.join(path, "slim_response_data.csv"))
-m = gl.recommender.item_similarity_recommender.create(data, user_id='user_id', item_id='place_id', target="response", user_data=None, item_data=None, nearest_items=None, similarity_type='pearson', training_method='auto', threshold=0.001, only_top_k=100, random_seed=0, verbose=True)
-recs = m.recommend()
-print recs
+# path = os.path.dirname(os.path.abspath(__file__))
+# data = gl.SFrame.read_csv(os.path.join(path, "slim_response_data.csv"))
+# m = gl.recommender.item_similarity_recommender.create(data, user_id='user_id', item_id='place_id', target="response", user_data=None, item_data=None, nearest_items=None, similarity_type='pearson', training_method='auto', threshold=0.001, only_top_k=100, random_seed=0, verbose=True)
+# recs = m.recommend()
+# print recs
 
-# this is for local export
-recs.export_csv(os.path.join(path, "new_csv_file.csv"),delimiter=",",line_terminator='\n', header=True)
+# # this is for local export
+# recs.export_csv(os.path.join(path, "new_csv_file.csv"),delimiter=",",line_terminator='\n', header=True)
 
 # for accessing AWS keys in .env file
 dotenv_path = join(dirname(__file__),'.env')
@@ -29,7 +29,7 @@ conn = S3Connection(access_key, secret_key)
 bucket = conn.get_bucket('niche-travel')
 
 user_recs = bucket.get_key('user_recs.csv')
-user_recs = user_recs.generate_url(30, method="GET")
+user_recs = user_recs.generate_url(120, method="GET")
 
 data = gl.SFrame.read_csv(user_recs)
 print data
--- lib/tasks/task.rake
+++ lib/tasks/task.rake
@@ -3,8 +3,7 @@ task write_s3: :environment do
 end
 
 task run_py: :environment do
-  py_path = `which python`
-  exec("#{py_path} #{Rails.root}/lib/recommender/py_recommender.py")
+  exec("python #{Rails.root}/lib/recommender/py_recommender.py")
 end
 
 task write_user_recs: :environment do
